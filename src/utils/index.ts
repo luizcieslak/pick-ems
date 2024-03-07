a75ab1b4d5eaa7395734643257e885c0a97fb33e
Luiz <cieslakluiz@gmail.com>
2024-03-06 23:19:17 -0300

    utils export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { mapTable, Table, concatTables, appendTable, toMarkdown } from './tables
 export { getBrowserInstance, closeBrowser, navigateTo } from './browser'
 export { llm } from './llm'
 export { distinct } from './distinct'
+export { fileExists } from './path'
